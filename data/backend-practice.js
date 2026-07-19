b78d9d78ef1e09d8e35f739ac289ac7cf5bf254d
bamoinweb <baranchr87@gmail.com>
2026-07-19 19:40:26 +0330

    Update backend-practice.js

--- data/backend-practice.js
+++ data/backend-practice.js
@@ -2,5 +2,5 @@ const xhr=new XMLHttpRequest();
 xhr.addEventListener('load',()=>{
    console.log(xhr.response);
 })
-xhr.open('get','http://localhost:3000/products');
+xhr.open('get','https://amazon-backend-1-konm.onrender.com/products');
 xhr.send();
